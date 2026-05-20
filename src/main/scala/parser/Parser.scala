0a6916dc8fc0fae71f9eb6a14215a91a036ad967
Dyumes <76482455+Dyumes@users.noreply.github.com>
2026-05-20 21:32:22 +0200

    small changes for tests

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -100,9 +100,9 @@ object Parser:
       case _ => throw expected("term")
 
   private def conditional(using Context): Result[Syntax[TermTree.Conditional]] =
-    take(Token.`if`, "if").and { start =>
-      term.andDiscard(take(Token.`then`, "then")).and { condition =>
-        term.andDiscard(take(Token.`else`, "else")).and { success =>
+    take(Token.`if`, "'if'").and { start =>
+      term.andDiscard(take(Token.`then`, "'then'")).and { condition =>
+        term.andDiscard(take(Token.`else`, "'else'")).and { success =>
           term.map { failure =>
             Syntax(
               TermTree.Conditional(condition, success, failure), start.span.extendedToCover(failure.span)
@@ -113,9 +113,9 @@ object Parser:
     }
 
   private def binding(using Context): Result[Syntax[TermTree.Binding]] =
-    take(Token.let, "let").and { start =>
-      termIdentifier.andDiscard(take(Token.equal, "=")).and { name =>
-        term.andDiscard(take(Token.semicolon, ";")).and { initializer =>
+    take(Token.let, "'let'").and { start =>
+      termIdentifier.andDiscard(take(Token.equal, "'='")).and { name =>
+        term.andDiscard(take(Token.semicolon, "';'")).and { initializer =>
           term.map { body =>
             Syntax(
               TermTree.Binding(name, initializer, body), start.span.extendedToCover(body.span)
